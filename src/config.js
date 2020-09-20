f65eafbb9a2736e29612665fe2c4bde7306ab8df
codingbeginner19 <60525942+codingbeginner19@users.noreply.github.com>
2020-09-20 05:39:29 -0400

    md

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
     process.env.DATABASE_URL ||
-    "postgresql://codingbeginner19@localhost/neverlostdb",
+    "postgres://xftmndqpsodrlt:ffdcf5d20b76aeeb046f6b1fd114a25cda93e536f0fb25c5890eee5df6328ab5@ec2-52-200-134-180.compute-1.amazonaws.com:5432/dfqkeg0tu2g84b",
 };
