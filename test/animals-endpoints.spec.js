e7bd4b879a0401607d850ac26aba71a78f6c2464
codingbeginner19 <60525942+codingbeginner19@users.noreply.github.com>
2020-09-22 17:26:30 -0400

    put in first id test

--- test/animals-endpoints.spec.js
+++ test/animals-endpoints.spec.js
@@ -1,6 +1,7 @@
 const { expect } = require("chai");
 const knex = require("knex");
 const app = require("../src/app");
+const { makeAnimalsArray } = require("./fixtures/animals.fixtures");
 
 describe("Animals Endpoints", function () {
   let db;
@@ -34,5 +35,36 @@ describe("Animals Endpoints", function () {
           "in_shelter"
         );
       });
+  });
+});
+describe(`GET /animals/:animal_id`, () => {
+  context("Given there are animals in the database", () => {
+    const testAnimals = makeAnimalsArray();
+
+    let db;
+
+    before("make knex instance", () => {
+      db = knex({
+        client: "pg",
+        connection: process.env.TEST_DATABASE_URL,
+      });
+      app.set("db", db);
+    });
+
+    it("responds with 200 and the specified animal", () => {
+      return supertest(app).get(`/animals/1`).expect(200, {
+        id: 1,
+        name: "g",
+        zip_code: "22042",
+        species: "human",
+        breed: "god",
+        color: "bleach",
+        age: 33,
+        gender: "Male",
+        description: "first user",
+        is_lost: true,
+        in_shelter: null,
+      });
+    });
   });
 });
---
+++ test/fixtures/animals.fixtures.js
@@ -0,0 +1,78 @@
+function makeAnimalsArray() {
+  return [
+    {
+      id: 1,
+      name: "205Z",
+      species: "First test post!",
+      breed: "How-to",
+      color:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?",
+      age: 1,
+      gender: "fdsa",
+      description: "How-to",
+      zip_code: "How-to",
+      in_distress: false,
+      in_shelter: 0,
+    },
+    {
+      id: 2,
+      name: "205Z",
+      species: "First test post!",
+      breed: "How-to",
+      color:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?",
+      age: 1,
+      gender: "fdsa",
+      description: "How-to",
+      zip_code: "How-to",
+      in_distress: false,
+      in_shelter: 0,
+    },
+    {
+      id: 3,
+      name: "205Z",
+      species: "First test post!",
+      breed: "How-to",
+      color:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?",
+      age: 4,
+      gender: "fdsa",
+      description: "How-to",
+      zip_code: "How-to",
+      in_distress: false,
+      in_shelter: 0,
+    },
+    {
+      id: 4,
+      name: "205Z",
+      species: "First test post!",
+      breed: "How-to",
+      color:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?",
+      age: 1,
+      gender: "fdsa",
+      description: "How-to",
+      zip_code: "How-to",
+      in_distress: false,
+      in_shelter: 0,
+    },
+    {
+      id: 5,
+      name: "205Z",
+      species: "First test post!",
+      breed: "How-to",
+      color:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?",
+      age: 1,
+      gender: "fdsa",
+      description: "How-to",
+      zip_code: "How-to",
+      in_distress: false,
+      in_shelter: 0,
+    },
+  ];
+}
+
+module.exports = {
+  makeAnimalsArray,
+};
